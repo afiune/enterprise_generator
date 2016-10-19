0a7e3dd08e00eb5fdc58359ae06b9547a2565aab
Salim Afiune <afiune@chef.io>
2016-10-19 16:45:08 -0400

    Initial commit
    Signed-off-by: Salim Afiune <afiune@chef.io>

---
+++ metadata.rb
@@ -0,0 +1,5 @@
+name             'enterprise_generator'
+description      'Custom code generator cookbook for use with ChefDK'
+long_description 'Custom code generator cookbook for use with ChefDK'
+version          '0.1.0'
+
